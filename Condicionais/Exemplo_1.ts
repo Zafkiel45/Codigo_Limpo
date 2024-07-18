007b68582644bff1ff16ef9aecfd83b0db28ae57
Murilo Silva <112777858+Zafkiel45@users.noreply.github.com>
2024-07-18 00:34:06 -0300

    Update Exemplo_1.ts

--- Condicionais/Exemplo_1.ts
+++ Condicionais/Exemplo_1.ts
@@ -18,3 +18,22 @@ function InputFlag(flag: string[]):void {
 }
 
 InputFlag(process.argv);
+
+// Se fosse com if...else:
+
+#!/usr/bin/env node
+
+function InputFlag(flag: string[]): void {
+  if(flag[3] === 'upperCase') {
+    callSomeFunctionUpperCase();
+  } else if (flag[3] === 'lowerCase') {
+    callSomeFunctionLowerCase();
+  } else if (flag[3] === 'capitalize') {
+    callSomeFunctionCapitalizeCase();
+  } else {
+      console.error('ocorreu um erro: MENSAGEM DE ERRO');
+      process.exit(1);
+  }
+};
+
+InputFlag(process.argv);
